ad78501f2666e0345854ca3bbb8f1180899ba3da
lisboagustavo256-dev <lisboagustavo256@gmail.com>
2026-07-23 14:06:33 -0300

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,21 @@
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+
+const commands = [
+    new SlashCommandBuilder()
+        .setName("char")
+        .setDescription("Mostra a skin Roblox")
+        .addStringOption(option =>
+            option
+                .setName("nick")
+                .setDescription("Nick Roblox")
+                .setRequired(true)
+        )
+].map(command => command.toJSON());
+
+const rest = new REST({ version: "10" })
+    .setToken(process.env.TOKEN);
+
+rest.put(
+    Routes.applicationCommands(process.env.CLIENT_ID),
+    { body: commands }
+);
